8657da359467517e1cf390e5a4e95601cc176a89
agustinaviaggio <agustinaviaggio@gmail.com>
2024-08-08 17:01:12 -0300

    Added generation of two refraction index tensors (empty and full) if the sensing option is True.

--- net.py
+++ net.py
@@ -11,8 +11,13 @@ class Generator(nn.Module):
         self.thickness_l = params.thickness_l
         self.N_layers = params.N_layers
         self.M_materials = params.M_materials
-        self.n_database = params.n_database.view(1, 1, params.M_materials, -1) # 1 x 1 x number of mat x number of freq
-        
+        self.sensor = params.sensor
+        if self.sensor:
+            self.n_database_full = params.n_database_full.view(1, 1, params.M_materials, -1) # 1 x 1 x number of mat x number of freq
+            self.n_database_empty = params.n_database_empty.view(1, 1, params.M_materials, -1) # 1 x 1 x number of mat x number of freq
+        else:
+           self.n_database = params.n_database.view(1, 1, params.M_materials, -1) # 1 x 1 x number of mat x number of freq
+                
         self.FC = nn.Sequential(
             nn.Linear(self.noise_dim, self.N_layers * (self.M_materials + 1)),
             nn.BatchNorm1d(self.N_layers * (self.M_materials + 1))
@@ -26,6 +31,15 @@ class Generator(nn.Module):
         X = net[:, :, 1:]
         
         P = F.softmax(X * alpha, dim = 2).unsqueeze(-1) # batch size x number of layer x number of mat x 1
+        
+        if self.sensor:
+            refractive_indices_full = torch.sum(P * self.n_database_full, dim=2) # batch size x number of layer x number of freq
+            refractive_indices_empty = torch.sum(P * self.n_database_empty, dim=2) # batch size x number of layer x number of freq
+            return (thicknesses, refractive_indices_empty, refractive_indices_full, P.squeeze())
+        else:
+            refractive_indices = torch.sum(P * self.n_database, dim=2) # batch size x number of layer x number of freq
+            return (thicknesses, refractive_indices, P.squeeze())
+        
         refractive_indices = torch.sum(P * self.n_database, dim=2) # batch size x number of layer x number of freq
         
         return (thicknesses, refractive_indices, P.squeeze())
@@ -69,8 +83,13 @@ class ResGenerator(nn.Module):
         self.thickness_l = params.thickness_l
         self.N_layers = params.N_layers
         self.M_materials = params.M_materials
-        self.n_database = params.n_database.view(1, 1, params.M_materials, -1) # 1 x 1 x number of mat x number of freq
-        
+        self.sensor = params.sensor
+        if self.sensor:
+            self.n_database_full = params.n_database_full.view(1, 1, params.M_materials, -1) # 1 x 1 x number of mat x number of freq
+            self.n_database_empty = params.n_database_empty.view(1, 1, params.M_materials, -1) # 1 x 1 x number of mat x number of freq
+        else:
+           self.n_database = params.n_database.view(1, 1, params.M_materials, -1) # 1 x 1 x number of mat x number of freq
+                
         self.initBLOCK = nn.Sequential(
             nn.Linear(self.noise_dim, self.res_dim),
             nn.LeakyReLU(0.2),
@@ -105,6 +124,11 @@ class ResGenerator(nn.Module):
         X = net[:, :, 1:]
         
         P = F.softmax(X * alpha, dim = 2).unsqueeze(-1) # batch size x number of layer x number of mat x 1
-        refractive_indices = torch.sum(P * self.n_database, dim=2) # batch size x number of layer x number of freq
         
-        return (thicknesses, refractive_indices, P.squeeze())
+        if self.sensor:
+            refractive_indices_full = torch.sum(P * self.n_database_full, dim=2) # batch size x number of layer x number of freq
+            refractive_indices_empty = torch.sum(P * self.n_database_empty, dim=2) # batch size x number of layer x number of freq
+            return (thicknesses, refractive_indices_empty, refractive_indices_full, P.squeeze())
+        else:
+            refractive_indices = torch.sum(P * self.n_database, dim=2) # batch size x number of layer x number of freq
+            return (thicknesses, refractive_indices, P.squeeze())
