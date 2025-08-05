b90a0de92dc8a047d110dbe1d586bf57cf41bc71
urquiagonzalo <urquiagonzalo@gmail.com>
2025-08-05 15:06:27 -0300

    Update GLOnet_thinfilm.py

--- GLOnet_thinfilm.py
+++ GLOnet_thinfilm.py
@@ -259,7 +259,12 @@ class GLOnet():
         
         #lambdas = (2 * math.pi / self.k).cpu().numpy()
         lambdas = (2 * math.pi / self.k).detach().cpu().numpy()
-        led_x_ldr = self.to_cuda_if_available(torch.from_numpy(self.led_spline(lambdas) * self.ldr_spline(lambdas)))
+
+        led_vals = self.led_spline(lambdas)
+        ldr_vals = self.ldr_spline(lambdas)
+        led_x_ldr = self.to_cuda_if_available(torch.from_numpy(led_vals * ldr_vals).float())
+        
+        #led_x_ldr = self.to_cuda_if_available(torch.from_numpy(self.led_spline(lambdas) * self.ldr_spline(lambdas)))
 
         #Corrección GU (VER):  en la siguiente línea se está tratando de hacer tratando de hacer un producto matricial con torch.matmul() 
         # entre dos tensores que no tienen dimensiones compatibles. reflection_empty.squeeze() tiene shape [8000000, 2]
